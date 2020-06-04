7b41549771d18ce09e9769549463fadb3d6f0396
gabodevelops <gabodevelops@users.noreply.github.com>
2020-06-04 11:07:19 -0500

    Test Site

---
+++ DriveBody&Paint-AlphaVersion/assets/js/modal.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+$(".tabs").click(function(){
+
+$(".tabs").removeClass("active");
+$(".tabs h6").removeClass("font-weight-bold");
+$(".tabs h6").addClass("text-muted");
+$(this).children("h6").removeClass("text-muted");
+$(this).children("h6").addClass("font-weight-bold");
+$(this).addClass("active");
+
+current_fs = $(".active");
+
+next_fs = $(this).attr('id');
+next_fs = "#" + next_fs + "1";
+
+$("fieldset").removeClass("show");
+$(next_fs).addClass("show");
+
+current_fs.animate({}, {
+step: function() {
+current_fs.css({
+'display': 'none',
+'position': 'relative'
+});
+next_fs.css({
+'display': 'block'
+});
+}
+});
+});
+
+});
